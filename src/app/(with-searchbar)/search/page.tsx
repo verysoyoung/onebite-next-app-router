87de2821af12250c6e29b48d1b0f1dd25bff673a
soyoung <duddlwwkd1@gmail.com>
2024-11-01 22:32:56 +0900

    navigation - useRouter

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -1,8 +1,17 @@
+import ClientComponent from '@/components/client-component'
+
 export default async function Page({
   searchParams,
 }: {
   searchParams: Promise<{ q: string }>
 }) {
   const { q } = await searchParams
-  return <div>Search Page : {q}</div>
+  return (
+    <div>
+      Search Page : {q}
+      <ClientComponent>
+        <></>
+      </ClientComponent>
+    </div>
+  )
 }
--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -1,8 +1,17 @@
+import ClientComponent from '@/components/client-component'
+
 export default async function Page({
   params,
 }: {
   params: Promise<{ id: string }>
 }) {
   const { id } = await params //동기적으로 가져옴
-  return <div>Book/[{id}] page</div>
+  return (
+    <div>
+      Book/[{id}] page
+      <ClientComponent>
+        <></>
+      </ClientComponent>
+    </div>
+  )
 }
