329531593e5a630b765f3eedf3c2fcb0e9afc473
soyoung <duddlwwkd1@gmail.com>
2024-11-17 09:42:11 +0900

    Skeleton

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -3,6 +3,8 @@ import style from './page.module.css'
 import { BookData } from '@/types'
 import { delay } from '@/util/delay'
 import { Suspense } from 'react'
+import BookItemSkeleton from '@/components/skeleton/book-item-skeleton'
+import BookListSkeleton from '@/components/skeleton/book-list-skeleton'
 
 // export const dynamic = 'force-dynamic' //app router 에서는 컴포넌트에서 세밀하게 dynamic, static 을 설정해주기 때문에 해당 옵션 사용 자제
 // test 할 수 있음
@@ -56,13 +58,13 @@ export default async function Home() {
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        <Suspense fallback={<div>추천도서 로딩...</div>}>
+        <Suspense fallback={<BookListSkeleton count={3} />}>
           <RecoBooks />
         </Suspense>
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        <Suspense fallback={<div>모든도서 로딩...</div>}>
+        <Suspense fallback={<BookListSkeleton count={10} />}>
           <AllBooks />
         </Suspense>
       </section>
