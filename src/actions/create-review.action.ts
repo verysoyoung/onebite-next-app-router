6df50dfb72cdd0d5846b6ea7fcaf1c10209927a8
soyoung <duddlwwkd1@gmail.com>
2024-11-26 23:02:00 +0900

    클라이언트 컴포넌트에서 서버액션 useActionState 사용!

--- src/actions/create-review.action.ts
+++ src/actions/create-review.action.ts
@@ -1,16 +1,21 @@
 'use server'
 
-import { revalidatePath, revalidateTag } from 'next/cache'
+import { delay } from '@/util/delay'
+import { revalidateTag } from 'next/cache'
 
-export async function createReviewAction(formData: FormData) {
+export async function createReviewAction(_: any, formData: FormData) {
   const bookId = formData.get('bookId')?.toString()
   const content = formData.get('content')?.toString()
   const author = formData.get('author')?.toString()
 
   if (!bookId || !content || !author) {
-    return
+    return {
+      status: false,
+      error: '리뷰내용과 작성자를 입력해주세요.',
+    }
   }
   try {
+    await delay(2000)
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`,
       {
@@ -18,7 +23,6 @@ export async function createReviewAction(formData: FormData) {
         body: JSON.stringify({ bookId, content, author }),
       }
     )
-    console.log(response.status)
     // revalidatePath(`/book/${bookId}`) //오직 서버에서만 호출가능, 모든 캐시 무효화
     //2. 특정 경로의 모든 동적 페이지를 재검증, 모든도서페이지 재검증
     // revalidatePath(`/book/[id]`,'page')
@@ -27,9 +31,18 @@ export async function createReviewAction(formData: FormData) {
     //4. 모든 데이터를 재검증
     // revalidatePath('/', 'layout')
     //5. 태그 기준, 데이터 캐시 재검증
+    if (!response.ok) {
+      throw new Error(response.statusText)
+    }
     revalidateTag(`review-${bookId}`)
+    return {
+      status: true,
+      error: '',
+    }
   } catch (err) {
-    console.error(err)
-    return
+    return {
+      status: false,
+      error: `리뷰 저장에 실패했습니다: ${err}`,
+    }
   }
 }
